71950232af8871e32bd7fd5b1f98b248ed73451c
ipincamp <support@nur-arifin.my.id>
2025-12-13 16:03:25 +0700

    feat: Refactor main app structure and set ZonationMapPage as home

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,37 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+import 'features/gis_map/presentation/pages/zonation_map_page.dart';
+
 void main() {
-  runApp(const MainApp());
+  runApp(const App());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Mobile GIS-IDW Cilongok',
+      debugShowCheckedModeBanner: kDebugMode,
+
+      // Konfigurasi Tema
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.teal,
+          brightness: Brightness.light,
+        ),
+        useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+          elevation: 2,
+          foregroundColor: Colors.white,
         ),
       ),
+
+      // Halaman yang pertama kali dibuka
+      home: const ZonationMapPage(),
     );
   }
 }
